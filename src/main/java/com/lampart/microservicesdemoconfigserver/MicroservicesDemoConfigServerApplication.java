8eae77c6e8199db242071b9346f754335cd3c4f7
[Lampart] Nguyen Van Hoai <van_hoai@lampart-vn.com>
2019-03-20 13:44:34 +0700

    add config

--- src/main/java/com/lampart/microservicesdemoconfigserver/MicroservicesDemoConfigServerApplication.java
+++ src/main/java/com/lampart/microservicesdemoconfigserver/MicroservicesDemoConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.lampart.microservicesdemoconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class MicroservicesDemoConfigServerApplication {
 
 	public static void main(String[] args) {
